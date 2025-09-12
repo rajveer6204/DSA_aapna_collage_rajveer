3366526bcaba68870234a43d0223e8e03bb0adc9
rajveer <singhveerraj232003@gmail.com>
2025-09-12 11:01:36 +0530

    DSA course pushing First code  calculator

---
+++ First_Code.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+ using namespace std;              //""
+ 
+int main(){
+    cout<<"First code in DSA in c++ \n";
+    int age;
+
+    cout<<"Enter the value : ";
+    cin >> age;
+    cout<<"your value is :"<< age;
+return 0;       
+}
---
+++ calculator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;     //""
+
+int main(){
+    int a=34;
+    int b =29;
+
+    cout<<"sum : " <<(a+b)<<endl;
+    cout<<"sub : " <<(a-b)<<endl;
+    cout<<"product : " <<(a*b)<<endl;
+    cout<<"division : " <<(a/b)<<endl;
+    cout<<"modulo : " <<(a%b)<<endl;
+  return 0;
+}
