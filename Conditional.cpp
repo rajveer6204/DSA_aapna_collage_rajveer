8aa0183b556f54aec39f45a9b214ec049713cede
Rajveer singh <singhveerraj2302@gmail.com>
2025-09-13 12:27:55 +0530

    conditional

---
+++ Conditional.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std ;
+
+int main(){
+// Conditional Statement 
+
+int n;
+cout<<"Enter the vlue of n :";
+cin>>n;
+
+if (n>=0){
+    cout<<"N is postive \n";
+}else{
+    cout<<"N is negtive \n";
+}
+return 0;
+}
